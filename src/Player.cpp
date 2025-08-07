821ea5e55590a24d9a499f766e56e93115a18ec0
Jayaram Anandan <68646971+jayaramanandan@users.noreply.github.com>
2025-08-07 11:57:32 +0000

    motion module added

--- src/Player.cpp
+++ src/Player.cpp
@@ -12,13 +12,10 @@ void Player::init() {
     const int frames[1][4] = {{1, 1, 14, 5}};
     this->addAnimation("idle", frames, 1);
     this->startAnimation("idle");
-    
+
     this->setOriginToObjectCentre();
     this->setPosition(20, 20);
-    this->setScale(10.0f, 10.0f);    
-
-    std::cout << this->getOrigin().x << std::endl;
-    std::cout << this->getOrigin().y << std::endl;
+    this->setScale(10.0f, 10.0f);
 }
 
 void Player::update() {
