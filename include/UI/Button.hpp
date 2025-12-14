3f3e72c61525c82d24e7f4b86edbbbd725f6699a
Jayaram Anandan <68646971+jayaramanandan@users.noreply.github.com>
2025-12-14 16:30:42 +0000

    gitignores

--- include/UI/Button.hpp
+++ include/UI/Button.hpp
@@ -17,12 +17,7 @@ namespace builder {
 
             sf::RectangleShape& getDrawable() override;
 
-<<<<<<< HEAD
-=======
-            void pollEvent(sf::Event& event) override;
->>>>>>> be58acab964884cb2bbe1c8d5dec536fc33f5530
             void update() override;
-
             
             virtual void onHover();
             virtual void onClick();
